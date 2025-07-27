fd00e22c6556cb5674f1f6d5b6d01c9bdee2d313
fatmaelghoul <fatouma_ghoul@live.com>
2025-07-27 11:11:05 +0200

    correction build

--- src/pages/contacts/Contact.jsx
+++ src/pages/contacts/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./contact.css";
+import "./Contact.css";
 
 function Contact() {
   const [formData, setFormData] = useState({ name: "", email: "", message: "" });
